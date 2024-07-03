32427a69a8836c43781a5831e3dbe24383250499
GaziRiad <ouhibimustapha36@gmail.com>
2024-07-03 23:00:07 +0100

    fixed loading error

--- components/LottieAnimation.jsx
+++ components/LottieAnimation.jsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function LottieAnimation({ src }) {
   console.log(src);
   const [dotLottie, setDotLottie] = useState(null);
+
+  useEffect(() => {
+    if (dotLottie) {
+      dotLottie.setFrame(30);
+      dotLottie.stop(); // Ensures the animation is preloaded but stopped
+    }
+  }, [dotLottie]);
 
   const dotLottieRefCallback = (dotLottieInstance) => {
     setDotLottie(dotLottieInstance);
